257f547fa9554597e5b1571cdc5cbbb28a542967
Tim Popov <popov_ta@mail.ru>
2020-08-27 22:50:28 +0300

    Импорт спика

--- CafeMashine/MainWindow.xaml.cs
+++ CafeMashine/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -31,5 +32,36 @@ namespace CafeMashine
         {
             new EditAvtomatsIngredients().ShowDialog();
         }
+
+        private void ImportListMenuItem_OnClick(object sender, RoutedEventArgs e)
+        {
+            string result = "";
+            foreach (Avtomat avtomat in DbProxy.Avtomats)
+            {
+                result += $"{avtomat.Id}:{avtomat.Value};";
+            }
+
+            result = result.Remove(result.Length - 1);
+
+            result += "#";
+
+            foreach (Ingredient ingredient in DbProxy.Ingredients)
+            {
+                result += $"{ingredient.Id}:{ingredient.Value};";
+            }
+            result = result.Remove(result.Length - 1);
+
+            StreamWriter sw = new StreamWriter("LIST.txt", false);
+            sw.WriteLine(result);
+            sw.Flush();
+            sw.Close();
+            MessageBox.Show("Текстовый файл LIST готов! \n Скопируйте его в телефон и выполните обновление списка на телефоне!");
+        }
+
+
+
+        //string data = "THExxQUICKxxBROWNxxFOX";
+
+        //    return data.Split(new string[] { "xx" }, StringSplitOptions.None);
     }
 }
